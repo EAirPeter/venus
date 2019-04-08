031ad8f68f4d4be8cfec9bcff26b31deb19ea491
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2019-04-08 02:51:41 -0700

    Adjustments to allow symbols for store offsets.

--- src/main/kotlin/venus/assembler/pseudos/Store.kt
+++ src/main/kotlin/venus/assembler/pseudos/Store.kt
@@ -15,30 +15,25 @@ object Store : PseudoWriter() {
     override operator fun invoke(args: LineTokens, state: AssemblerPassOne): List<LineTokens> {
         checkArgsLength(args, 4)
         val hasParens = args[3].startsWith('(')
-        var argsv = args
-
-        if (hasParens) {
-            argsv = listOf(args[0], args[1], args[2],
-                           args[3].substring(1, args[3].length - 1))
-        }
         val label = args[2]
+        val arg3 = if (hasParens) args[3] else args[3].substring(1, args[3].length - 1)
         try {
             userStringToInt(label)
             /* if it's a number, this is just an ordinary store instruction */
-            return listOf(argsv)
+            return listOf(listOf(args[0], args[1], label, arg3))
         } catch (e: NumberFormatException) {
             if (hasParens) {
                 state.addRelocation(ImmAbsStoreRelocator, state.getOffset(),
                                     label)
-                return listOf(argsv)
+                return listOf(listOf(args[0], args[1], "0", arg3))
             }
             /* assume it's a label */            
         }
 
-        val auipc = listOf("auipc", argsv[3], "0")
+        val auipc = listOf("auipc", arg3, "0")
         state.addRelocation(PCRelHiRelocator, state.getOffset(), label)
 
-        val store = listOf(args[0], args[1], "0", argsv[3])
+        val store = listOf(args[0], args[1], "0", arg3)
         state.addRelocation(PCRelLoStoreRelocator, state.getOffset() + 4, label)
 
         return listOf(auipc, store)
